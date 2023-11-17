8a5f16d3731ca30bb086f7bec4f7629fdfc848d8
Denisa Hazakova <denisahazakova@gmail.com>
2023-11-17 09:45:23 +0100

    Your descriptive commit message

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, id }) => {
     <div className="tc bg-lightest-blue navy dib ma2 br3 pa3 grow bw2 shadow-5">
       <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
       <div>
-        <h2>{name}</h2>
+        <h4>{name}</h4>
         <p>{email}</p>
       </div>
     </div>
